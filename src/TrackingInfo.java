b1a6628e618c4eebe24f0807820079988f2a59bb
MichaelGSanchez <michaelgregorysanchez@gmail.com>
2019-04-09 21:36:02 -0600

    Shipping and Tracking attempt

---
+++ src/TrackingInfo.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+public class TrackingInfo {
+  private Date recorded;
+
+  private String details;
+
+  public TrackingInfo(Date recorded, String details) {
+    this.recorded = recorded;
+    this.details = details;
+  }
+
+  public Date getRecorded() {
+    return recorded;
+  }
+
+  public String getDetails() {
+    return details;
+  }
+}
